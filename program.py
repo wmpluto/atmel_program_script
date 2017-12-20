33aceab74bf34d67fce62fa9d0b4a92b61f51463
Pluto Wang <wmpluto@gmail.com>
2017-12-20 12:22:44 +0800

    Read Signature OK

--- program.py
+++ program.py
@@ -1,15 +1,26 @@
 #! /usr/bin/python3
 
-import os
+import os, time, subprocess
 
 class AtProgram:
     # os.system('notepad')
-    atprogram = r'"C:Program Files (x86)\Atmel\Studio\7.0\atbackend\atprogram.exe"'
-    device = "ATA5702M322"
+    tool = 'atmelice'
 
-    def __init__(self, device, interface):
+    def __init__(self, device, interface, atprogram):
         self.device = device
-        self.interface = interfaces
+        self.interface = interface
+        self.prefix = atprogram
+
+        self.preprocess()
+
+    def program(self, file):
+        print("File Size: %.1fK" % (int(os.path.getsize(file))/1024))
+        print("Date Motified: ", time.asctime(time.localtime(os.path.getmtime(file))))
+
+        if True:
+            self.flash(file)
+        else:
+            self.eeprom(file)
 
     def flash(self, file):
         # atprogram -t avrispmk2 -i ISP -d atmega128rfr2 -xr -cl 2mhz program -c --verify -f foo61.hex
@@ -30,17 +41,42 @@ class AtProgram:
 
     def signature(self):
         # atprogram.exe -t atmelice -i ISP -d ATA5702M322 info --signature
-        pass
-        
-def main():
-    print("ATA5702 Program Tool")
+        arguments = " info " + " --signature "
+        cmd = self.prefix + arguments
+        self.actuator(cmd)
 
+    def preprocess(self):
+        self.prefix += " -t %s " % self.tool
+        self.prefix += " -i %s " % self.interface
+        self.prefix += " -d %s " % self.device
+
+    def actuator(self, cmd):
+        ps = subprocess.Popen(cmd, stdout=subprocess.PIPE)
+        ps.wait()
+        if ps.returncode == 0:
+            print(str(ps.stdout.read(), encoding="utf-8").split('\r\n')[1])
+        else:
+            pass
+
+    def log(self):
+        pass
+
+def main():
+    device = "ATA5702M322"
+    interface = "ISP"
+    atprogram = r'"C:\Program Files (x86)\Atmel\Studio\7.0\atbackend\atprogram.exe "'
+
+    print("%s Program Tool using %s" % (device, interface))
+
+    programer = AtProgram(device, interface, atprogram)
     while True:
-        cmd = input("Enter CMD: ")
+        cmd = input("\nEnter CMD: ")
         if cmd == "erase":
             pass
-        elif os.path.exists(cmd):
-            print(cmd)
+        elif cmd == "signature":
+            programer.signature()
+        elif os.path.isfile(cmd) and os.path.exists(cmd):
+            programer.program(cmd)
         else:
             print("Unrecognized CMD\n")
 
